41cad464b6ff30990c7cef7547209e3d66fc8848
Keith Tan <keithtanzihao@gmail.com>
2022-05-15 22:49:02 +0800

    Initial commit mainpage components

---
+++ src/components/mainpage/CardSlider.js
@@ -0,0 +1,47 @@
+import axios from "axios";
+import React, { Fragment, useState, useEffect } from "react";
+
+import styles from "../../styles/main.module.scss";
+
+export default function CardSlider(props) {
+  const [animationIndex, setAnimationIndex] = useState(0);
+
+  const executeAnimations = () => {
+    if (animationIndex === 2) {
+      setAnimationIndex(0);
+    } else {
+      setAnimationIndex(animationIndex + 1);
+    }
+  };
+
+  return (
+    <div className={`${styles["cardSlider__ctn"]}`}>
+      <img
+        src={props.layer1Img}
+        className={animationIndex === 2 
+          ? `${styles["cardSlider__img--center"]}`
+          : `${styles["cardSlider__img--left"]}`
+        }
+        onClick={executeAnimations}
+      />
+      <img
+        src={props.layer2Img}
+        className={animationIndex === 1 
+          ? `${styles["cardSlider__img--center"]}`
+          : animationIndex < 2 
+          ? `${styles["cardSlider__img--right"]}`
+          : `${styles["cardSlider__img--hidden"]}`
+        }
+        onClick={executeAnimations}
+      />
+      <img
+        src={props.layer3Img}
+        className={animationIndex < 1 
+          ? `${styles["cardSlider__img--center"]}`
+          : `${styles["cardSlider__img--hidden"]}`
+        }
+        onClick={executeAnimations}
+      />
+    </div>
+  );
+}
---
+++ src/components/mainpage/ProductHighlight.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import React, { Fragment, useState, useEffect } from "react";
+
+import Button from "../ui/Button";
+
+import styles from "../../styles/main.module.scss";
+// A route for this later ?
+// import almondImg from "../../styles/vendors/images/almond.webp";
+// import handImg from "../../styles/vendors/images/hand.svg";
+
+export default function ProductHightlight() {
+  const BASE_URL = "https://wildfoodsbackend.herokuapp.com";
+
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const products = await axios.get(
+          BASE_URL + "/product/category_title/tahinis"
+        );
+        setProductData(products.data);
+      } catch (error) {
+        console.log("productCarousel useEffect problem");
+      }
+    };
+    getData();
+    setHasLoaded(true);
+  }, []);
+
+  return (
+    <div className={`${styles["productHighlight"]}`}>
+      <div className={`${styles["productHighlight__ctn--head"]}`}>
+        <h1>Wild like a</h1>
+        <h1>Nut??</h1>
+      </div>
+
+      <div className={`${styles["productHighlight__ctn--content"]}`}>
+        <div>
+          <div className={`${styles["productHighlight__ctn--productHead"]}`}>
+            <h1>Roasted</h1>
+            <h1>Almond</h1>
+          </div>
+          <p className={`${styles["productHighlight__ctn--productDesc"]}`}>
+            All the treasures are hidden in a chest. Respectively, our treasures
+            are enclosed in their Wild shell.
+          </p>
+          <Button
+            className={`${styles["button__mantra--discover"]}`}
+            content="Discover More !"
+          />
+        </div>
+
+        <div className={`${styles["productHighligh__imgCtn"]}`}>
+          {/* <img
+            className={`${styles["productHighlight__img--almond"]}`}
+            src={almondImg}
+          />
+          <img
+            className={`${styles["productHighlight__img--hand"]}`}
+            src={handImg}
+          /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
