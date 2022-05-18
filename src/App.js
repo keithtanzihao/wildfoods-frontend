deba4cf785804c3e148454681942a632d0610497
Keith Tan <keithtanzihao@gmail.com>
2022-05-18 16:55:54 +0800

    t

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import AuthProvider from "./context/auth-provider";
+import SidebarProvider from "./context/sidebarContext/sidebar-provider";
 
 // Components
 import Mainpage from "./components/mainpage/Mainpage";
@@ -12,39 +13,28 @@ import ProductPage from "./components/productpage/ProductPage";
 import ProductInfoCtn from "./components/productpage/ProductInfoCtn";
 import ProductInfo from "./components/productpage/ProductInfo";
 import OrderPage from "./components/orderpage/OrderPage";
-
-const BASE_URL = "https://wildfoodsbackend.herokuapp.com";
-
+ 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       let response = await axios.get(BASE_URL + "/admin/staff");
-  //       setIsAuthenticated(response.data.isAuthenticated);
-  //     } catch (error) {
-  //       console.log("App useeffect error");
-  //     }
-  //   }
-  //   getData();
-  // })
 
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="" element={<Mainpage />} />
-        <Route path="products" element={<ProductPage />} />
+      <SidebarProvider>
+        <Routes>
+          
+          <Route path="" element={<Mainpage />} />
+          <Route path="products" element={<ProductPage />} />
 
-        <Route path="product/" element={<ProductInfoCtn/>}>
-          <Route path=":id" element={<ProductInfo/>}/>
-        </Route>
+          <Route path="product/" element={<ProductInfoCtn />}>
+            <Route path=":id" element={<ProductInfo />} />
+          </Route>
 
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />          
 
-        <Route path="order" element={<OrderPage />}/> 
-      </Routes>
+          <Route path="order" element={<OrderPage />} />
+
+        </Routes>
+      </SidebarProvider>
     </AuthProvider>
   );
 }
